6da95f4f75c4540395aee85eb0e86867d82de08a
TheAbzo <soon-ab@hotmail.com>
2018-03-05 20:02:19 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,31 @@ import java.net.URL;
 import java.util.Scanner;
 import java.io.IOException;
 import java.util.Vector;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Document;
+import org.jsoup.*;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        // write your code here
-        Vector<String> links = new Vector<>();
-       links = robotFetcher("https://twitter.com/robots.txt");//https://www.facebook.com/robots.txt
-        for(String lstitem : links){
+//        //  TESTS
 
-            System.out.println("List item: " + lstitem);
-        }
+//        Vector<String> links = new Vector<>();
+//       links = robotFetcher("https://twitter.com/robots.txt");//https://www.facebook.com/robots.txt
+//        for(String lstitem : links){
+//
+//            System.out.println("List item: " + lstitem);
+//        }
+//        Vector<String> links = new Vector<>();
+//        String abzo = getSiteMapLink("http://www.mbc.net/robots.txt");
+//        System.out.println("i am " + abzo);
+//      links = siteMapFetcher(abzo);
+//
+//        for(String lstitem : links){
+//
+//            System.out.println("List item: " + lstitem);
+//        }
 
     }
 
@@ -64,4 +76,54 @@ public class Main {
         //return after finishing the file
         return links;
     }
+
+
+
+    /**
+     * #@param robotLink the link of robot.txt
+     *
+     * @return string link of siteMap if found or -1 if not found
+     */
+    public static String getSiteMapLink(String robotLink) throws Exception
+    {
+        URL robotFile = new URL(robotLink);
+
+        String siteMap = "-1";
+
+        //read robot.txt line by line
+        Scanner robotScanner = new Scanner(robotFile.openStream());
+        while (robotScanner.hasNextLine()) {
+            String line = robotScanner.nextLine();
+
+            //search for disallow
+            if (line.contains("Sitemap:") || line.contains("sitemap:") ) //websites have either one
+
+               siteMap = line.toLowerCase().replaceAll("sitemap: ", "");
+        }
+        return siteMap;
+    }
+
+
+
+    /**
+     * #@param  xmlLink the link of sitemap
+     *
+     * @return string vector of links
+     */
+    public static Vector<String> siteMapFetcher(String xmlLink) throws Exception{
+
+        Vector<String> siteMapLinks = new Vector<>();
+        Document doc = Jsoup.connect(xmlLink).get();
+
+        //get the urls between loc  tag
+        Elements urls = doc.getElementsByTag("loc");
+
+        //saving the urls
+        for (Element url : urls) {
+            siteMapLinks.add(url.text());
+        }
+
+        return siteMapLinks;
+    }
+
 }
